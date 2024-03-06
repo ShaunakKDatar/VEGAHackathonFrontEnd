67a83c0d15eb7e7d738c3c9b55c8eb1e328889e9
Ghruank <ghruankkothare@gmail.com>
2024-03-06 19:58:50 +0530

    forum

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,13 +1,15 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../Components/Navbar/Navbar";
+import QueryForum from "../Components/QueryForum";
 
 
 export default function Root() {
     return (
       <>
       
-        <Navbar/>
-        <Outlet/>
+        {/* <Navbar/>
+        <Outlet/> */}
+        <QueryForum/>
         
       </>
     );

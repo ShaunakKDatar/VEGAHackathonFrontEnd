ff64efab4301691e0a8d3d3b814cd7e55ecef43c
zohervohra <zohervohra53@gmail.com>
2024-03-06 20:33:38 +0530

    Add AnnouncementPage and Announcements component

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,14 +1,26 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../Components/Navbar/Navbar";
+import MyCalendar from "../Components/MyCalender/MyCalendar";
 
+import AnnouncementPage from "../Components/Announcements/AnnouncementPage";
 
 export default function Root() {
     return (
       <>
       
         <Navbar/>
-        <Outlet/>
-        
+        {/* <Outlet/> */}
+      <div className="flex flex-row pt-[70px]">
+        <div style={{width : '60vw'}}>
+        <AnnouncementPage/>
+        </div>
+        <div style={{width : '40vw'}}>
+        <MyCalendar/> 
+        </div>
+  
+      
+      </div>
+      {/* <MyCalendar/> */}
       </>
     );
   }

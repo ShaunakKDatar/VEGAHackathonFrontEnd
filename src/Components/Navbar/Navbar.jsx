3c921bf6a6b6300a5c3ab46734dc3ee7a9d37bb2
Ghruank <ghruankkothare@gmail.com>
2024-03-07 03:58:41 +0530

    navbarprofile

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 import { useContext } from "react";
 import  { UserDataContext } from "../../Context/UserData";
+import './Navbar.css'
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
   const data = useContext(UserDataContext);
@@ -10,9 +11,9 @@ const Navbar = () => {
     setIsOpen(!isOpen);
   };
   return (
-    <div className="navbar bg-tertiary flex items-center justify-around fixed w-full z-30">
+    <div className="navbar bg-tertiary ">
 
-        <div className="companyname flex flex-col items-center justify-center">
+        <div className="companyname">
           <a href="/">
             <img src={"discord.png"} alt="" width={'50px'} />
           </a>
@@ -33,23 +34,23 @@ const Navbar = () => {
           </li>
         </ul>
 
-      <div className="profile dropdown flex items-center justify-around gap-[5px]"  onClick={toggleMenu}>
+      <div className="profile dropdown"  onClick={toggleMenu}>
         <img className="img1 rounded-full" width={'50px'} src={"photo_default.jpg"} alt="" />
         <p className="cursor-pointer" onClick={toggleMenu}>
           {(localStorage.getItem("X-auth-token"))?data.userData.username:"User"} ⤦
         </p>
         {isOpen && (
-          <div className="menu absolute top-[70px] bg-white  w-100 fixed bg-white border border-gray-300 p-4 rounded-lg shadow-md" style={{right: '2%', top: '10%'}}>
-            <div className="dropcontainer flex flex-col w-100">
-              <div className="profile flex flex-row items-center justify-between">
-                {/* <img src={"photo_default.jpg"} alt="" width="40px" /> */}
-                <p className="name font-semibold text-lg">Ghruank</p>
+          <div className="menu">
+            <div className="dropcontainer">
+              <div className="profile">
+                <img src={"photo_default.jpg"} alt="" width="40px" />
+                <p className="name">Ghruank</p>
               </div>
               <a href="">My profile</a>
-              <div className="line border-t-2 border-gray-400 my-10"></div>
+              <div className="line"></div>
               <a href="">Settings</a>
               <a href="">Notification</a>
-              <div className="line border-t-2 border-gray-400 my-10"></div>
+              <div className="line"></div>
               <Link to="/login" className="cursor-pointer" >login</Link>
             </div>
           </div>

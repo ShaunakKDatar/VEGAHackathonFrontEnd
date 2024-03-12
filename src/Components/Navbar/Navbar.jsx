154034da08f11e870d283aa3c9157c9d0a4c5103
Soham Sunil Margaj <sohammargaj55555@gmail.com>
2024-03-13 01:09:57 +0530

    Refactor Navbar component styling and Delete Navbar.css file

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -5,17 +5,17 @@ import  { UserDataContext } from "../../Context/UserData";
 import { IoIosLogIn } from "react-icons/io";
 import { IoIosSettings } from "react-icons/io";
 import { IoIosNotifications } from "react-icons/io";
-import './Navbar.css'
+// import './Navbar.css'
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
   const data = useContext(UserDataContext);
-  console.log(data);
+  // console.log(data);
 
   const toggleMenu = () => {
     setIsOpen(!isOpen);
   };
   return (
-    <div className="navbar bg-tertiary ">
+    <div className="navbar bg-tertiary flex items-center justify-around ">
 
         <div className="companyname">
           <Link to="/">
@@ -39,16 +39,16 @@ const Navbar = () => {
           </li>
         </ul>
 
-      <div className="profile dropdown"  onClick={toggleMenu}>
+      <div className="profile dropdown flex items-center gap-2"  onClick={toggleMenu}>
         <img className="img1 rounded-full" width={'50px'} src={"photo_default.jpg"} alt="" />
         <p className="cursor-pointer" onClick={toggleMenu}>
           {(localStorage.getItem("X-auth-token"))?data.userData.username:"User"} ⤦
         </p>
         {isOpen && (
-          <div className="menu">
-            <div className="dropcontainer">
-              <div className="profile">
-                <img src={"photo_default.jpg"} alt="" width="40px" />
+          <div className="menu absolute bg-tertiary p-5 rounded-[5px] top-[70px] ">
+            <div className="dropcontainer flex flex-col gap-[3px] justify-center items-center">
+              <div className="profile flex items-center gap-[5px] ">
+                <img src={"photo_default.jpg"} className="rounded-full" alt="" width="40px" />
                 <p className="name">{(localStorage.getItem("X-auth-token"))?data.userData.username:"User"}</p>
               </div>
               <Link to="/profile">My profile</Link> 
